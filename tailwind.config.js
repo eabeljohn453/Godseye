42e0ad371f7a083488ab43a93c1b257bb79c3066
Eabel John <eabeljohn453@gmail.com>
2025-01-07 14:06:13 +0530

    project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "C:/Users/eabel/OneDrive/Desktop/project/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
